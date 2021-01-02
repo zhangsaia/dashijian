8e601647cd2ebce67b00392976c4505690649cd8
zhangsai <2578025442@qq.com>
2021-01-02 09:24:31 +0800

    bash: 初始化git仓库

---
+++ assets/js/baseAPI1.js
@@ -0,0 +1,18 @@
+$.ajaxPrefilter(function (options) {
+    options.url = 'http://ajax.frontend.itheima.net' + options.url
+
+
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+
+    options.complete = function (res) {
+        if (res.responseJSOM.status === 1 && res.responseJSON.message === '身份验证失败！') {
+            localStorage.removeItem('token')
+            location.href = '/login1.html'
+        }
+    }
+})
