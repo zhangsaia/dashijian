a9758329c4db4eff55a1c9153eaa80c4357c9268
zhangsai <2578025442@qq.com>
2021-01-03 16:38:35 +0800

    完成了个人中心的开发

--- assets/js/index1.js
+++ assets/js/index1.js
@@ -6,8 +6,8 @@ $(function () {
             //do something
             localStorage.removeItem('token')
             location.href = '/login1.html'
-            layer.close(index);
-          });
+            layer.close(index)
+          })
     })
 })
   
@@ -16,26 +16,27 @@ function getUserInfo() {
     $.ajax({
         method: 'GET',
         url: '/my/userinfo',
-        /* headers: {
-            Authorization:localStorage.getItem('token') || ''
-        }, */
+        // headers: {
+        //     Authorization:localStorage.getItem('token') || ''
+        // },
         success: function (res) {
             // console.log(res);
             if (res.status !== 0) {
-                return layui.layer.msg('获取用户信息失败')
+                return layui.layer.msg('获取用户信息失败！')
             }
             renderAvatar(res.data)
-        }/* ,
-        complete: function (res) {
-            if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败') {
-                localStorage.removeItem('token')
-                location.href = '/login1.html'
-            }
-        } */
+        }
+        // },
+        // complete: function (res) {
+        //     if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败') {
+        //         localStorage.removeItem('token')
+        //         location.href = '/login1.html'
+        //     }
+        // }
     })
 }
 function renderAvatar(user) {
-    var name = user.username || user.nickname
+    var name = user.nickname || user.username
     $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
     if (user.user_pic !== null) {
         $('.layui-nav-img').attr('src', user.user_pic).show()
